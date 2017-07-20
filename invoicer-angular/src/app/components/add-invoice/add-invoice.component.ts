a4140ac6b085af0c2dd47bed086916f65bd35f60
Belchenkov <u608110@gmail.com>
2017-07-20 08:31:39 +0300

    Add Invoice

--- invoicer-angular/src/app/components/add-invoice/add-invoice.component.ts
+++ invoicer-angular/src/app/components/add-invoice/add-invoice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from "../../services/customer.service";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: 'app-add-invoice',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddInvoiceComponent implements OnInit {
 
-  constructor() { }
+  customer;
+  service;
+  price;
+  status;
+  due;
+
+  constructor(
+    private customerService: CustomerService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+  }
+
+  onAddSubmit() {
+    this.customer = this.route.snapshot.params['customer_id'];
+
+
+    let invoice = {
+      customer: this.customer,
+      service: this.service,
+      price: this.price,
+      status: this.status,
+      due: this.due
+    };
+
+    this.customerService.saveInvoice(invoice).subscribe(invoice => {
+      this.router.navigate(['/']);
+    });
   }
 
 }
